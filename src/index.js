daaedd764fa91fb911c9d8653b29a3c0aad318de
Matt Magurany <matt.magurany@genesys.com>
2024-09-12 11:33:43 -0400

    More cleanup

--- src/index.js
+++ src/index.js
@@ -2,87 +2,104 @@ import { registerElements } from "genesys-spark-components";
 
 const clientId = 'a64ab37d-b6f4-4c67-bb02-425255a7cc09';
 const redirectURI = encodeURI('http://localhost:7890/');
-const params = new URLSearchParams(window.location.search);
-const authCode = params.get('code');
 
-if (authCode) {
-  fetch('https://login.inindca.com/oauth/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams({
-      grant_type: 'authorization_code',
-      code: authCode,
-      redirect_uri: redirectURI,
-      client_id: clientId,
-      code_verifier: window.sessionStorage.getItem('code-verifier')
-    }).toString()
-  }).then(async response => {
-    const info = await response.json();
+async function exchangeCodeForToken() {
+  const params = new URLSearchParams(location.search);
+  const authCode = params.get('code');
 
-    window.sessionStorage.setItem('access-token', info.access_token);
-    window.location.replace('/');
-  }).finally(() => {
-    window.sessionStorage.removeItem('code-verifier');
-  });
+  if (authCode) {
+    const tokenResponse = await fetch('https://login.inindca.com/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: authCode,
+        redirect_uri: redirectURI,
+        client_id: clientId,
+        code_verifier: sessionStorage.getItem('code-verifier')
+      }).toString()
+    });
+
+    const tokenInfo = await tokenResponse.json();
+
+    sessionStorage.setItem('access-token', tokenInfo.access_token);
+    sessionStorage.removeItem('code-verifier');
+    location.replace('/');
+
+    return true;
+  }
+
+  return false;
 }
-
-registerElements();
 
 function generateCodeVerifier() {
   const a = new Uint8Array(43);
-  window.crypto.getRandomValues(a);
+  crypto.getRandomValues(a);
   return Array.from(a, n => n.toString(36)).join('');
 }
 
 async function createCodeChallenge(verifier) {
   const encoder = new TextEncoder();
   const data = encoder.encode(verifier);
-  const hashedBuf = await window.crypto.subtle.digest('SHA-256', data);
+  const hashedBuf = await crypto.subtle.digest('SHA-256', data);
   const challenge = btoa(String.fromCharCode(...(new Uint8Array(hashedBuf)))).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 
   return challenge;
 }
 
-const codeVerifier = window.sessionStorage.getItem('code-verifier') || generateCodeVerifier();
-let codeChallenge;
+async function init() {
+  const codeVerifier = sessionStorage.getItem('code-verifier') || generateCodeVerifier();
+  
+  sessionStorage.setItem('code-verifier', codeVerifier);
+  document.getElementById('code-verifier').innerText = codeVerifier;
 
-window.sessionStorage.setItem('code-verifier', codeVerifier);
-document.getElementById('code-verifier').value = codeVerifier;
+  const codeChallenge = await createCodeChallenge(codeVerifier);
 
-createCodeChallenge(codeVerifier).then(challenge => {
-  codeChallenge = challenge;
-  document.getElementById('code-challenge').value = challenge;
-});
-
-document.getElementById('request-token').addEventListener('click', () => {
-  window.location.assign(`https://login.inindca.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectURI}&code_challenge=${codeChallenge}&code_challenge_method=S256`);
-});
-
-const accessToken = window.sessionStorage.getItem('access-token');
-const accessTokenInput = document.getElementById('access-token')
-accessTokenInput.value = accessToken;
-
-const meResponseInput = document.getElementById('me-response');
-
-if (accessToken) {
-  fetch('https://api.inindca.com/api/v2/users/me', {
-    headers: {
-      'Authorization': `bearer ${accessToken}`
-    }
-  }).then(async response => {
-    const me = await response.json();
-
-    meResponseInput.innerText = JSON.stringify(me, null, 2);
+  document.getElementById('code-challenge').innerText = codeChallenge;
+  document.getElementById('request-token').addEventListener('click', () => {
+    const params = new URLSearchParams({
+      client_id: clientId,
+      response_type: 'code',
+      redirect_uri: redirectURI,
+      code_challenge: codeChallenge,
+      code_challenge_method: 'S256'
+    });
+    location.assign(`https://login.inindca.com/oauth/authorize?${params.toString()}`);
   });
 
-  const clearButton = document.getElementById('clear-token');
-  clearButton.disabled = false;
-  clearButton.addEventListener('click', () => {
-    window.sessionStorage.removeItem('access-token');
-    accessTokenInput.value = '';
-    meResponseInput.innerText = "";
-    clearButton.disabled = true;
-  })
+  const accessToken = sessionStorage.getItem('access-token');
+
+  if (accessToken) {
+    const accessTokenEl = document.getElementById('access-token');
+    accessTokenEl.innerText = accessToken;
+
+    const meResponse = await fetch('https://api.inindca.com/api/v2/users/me', {
+      headers: {
+        'Authorization': `bearer ${accessToken}`
+      }
+    });
+
+    const me = await meResponse.json();
+
+    const meResponseEl = document.getElementById('me-response');
+    meResponseEl.innerText = JSON.stringify(me, null, 2);
+
+    const clearButton = document.getElementById('clear-token');
+    clearButton.disabled = false;
+    clearButton.addEventListener('click', () => {
+      sessionStorage.removeItem('access-token');
+      accessTokenEl.innerText = 'Not authenticated';
+      meResponseEl.innerText = 'The public API response will display here';
+      clearButton.disabled = true;
+    });
+  }
+}
+
+const exchanged = await exchangeCodeForToken();
+
+if (!exchanged) {
+  registerElements();
+  await init();
 }
